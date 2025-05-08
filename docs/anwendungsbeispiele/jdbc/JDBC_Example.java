c7a733550eb16fc3c807b980aadc1f15d4eeac41
MasterOfDevilLP <flosue2017@gmail.com>
2025-05-08 13:58:22 +0200

    JDBC documentation fertig (#17)

--- docs/anwendungsbeispiele/jdbc/JDBC_Example.java
+++ docs/anwendungsbeispiele/jdbc/JDBC_Example.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.microsoft.sqlserver.jdbc.*;
 /*
@@ -79,6 +81,46 @@ public static void exampleQuery()
 
 }
 //end::exampleQuery()[]
+
+//tag::exampleParameterQuery()[]
+public static void exampleParameterQuery(HashMap<String, Integer> salesForWeek) throws SQLException
+{
+    String updateString = "UPDATE coffees SET sales = ? WHERE cof_name = ?";
+    String updateStatement = "UPDATE coffees SET total = total + ? WHERE cof_name = ?";
+    try (   PreparedStatement updateSales = conn.prepareStatement(updateString);
+            PreparedStatement updateTotal = conn.prepareStatement(updateStatement))
+    {
+        conn.setAutoCommit(false); // only full changes should be committed
+        for (Map.Entry<String, Integer> e : salesForWeek.entrySet())
+        {
+            updateSales.setInt(1, e.getValue().intValue()); // index starts at 1 (why ever)
+            updateSales.setString(2, e.getKey());
+            updateSales.executeUpdate();
+
+            updateTotal.setInt(1, e.getValue().intValue());
+            updateTotal.setString(2, e.getKey());
+            updateTotal.executeUpdate();
+            
+            conn.commit(); // changes get committed
+        }
+    } catch (SQLException e) // Exception handling
+    {
+        System.err.println(e.getMessage());
+        if (conn != null)
+        {
+            try
+            {
+                System.err.println("Transaction failed, rolling back...");
+                conn.rollback();
+            } catch (SQLException excep)
+            {
+                System.err.println(excep.getMessage());
+                excep.printStackTrace();
+            }
+        }
+    }
+}
+//end::exampleParameterQuery()[]
 
     public static void main(String[] args) throws Exception
     {

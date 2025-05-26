dbf9f5fb296f724aafcab0d38534565fa04faa18
MasterOfDevilLP <flosue2017@gmail.com>
2025-05-26 13:59:20 +0200

    added the needed cascading (#60)

---
+++ src/backend/database/Cascading.sql
@@ -0,0 +1,21 @@
+BEGIN;
+	-- fix items wgid fk
+	ALTER TABLE items DROP CONSTRAINT items_wgid_fkey;
+	ALTER TABLE items ADD FOREIGN KEY (wgid) REFERENCES wg(wgid) ON DELETE CASCADE;
+
+	-- fix shoppinglists wgid fk
+	ALTER TABLE shoppinglists DROP CONSTRAINT shoppinglists_wgid_fkey;
+	ALTER TABLE shoppinglists ADD FOREIGN KEY (wgid) REFERENCES wg(wgid) ON DELETE CASCADE;
+
+	-- fix users wgid fk
+	ALTER TABLE users DROP CONSTRAINT users_wgid_fkey;
+	ALTER TABLE users ADD FOREIGN KEY (wgid) REFERENCES wg(wgid) ON DELETE SET NULL;
+	
+	-- fix itemchanges itemid fk
+	ALTER TABLE itemchanges DROP CONSTRAINT itemchanges_itemid_fkey;
+	ALTER TABLE itemchanges ADD FOREIGN KEY (itemid) REFERENCES items(itemid) ON DELETE CASCADE;
+
+	-- fix listchanges shoppinglistid fk
+	ALTER TABLE listchanges DROP CONSTRAINT listchanges_shoppinglistid_fkey;
+	ALTER TABLE listchanges ADD FOREIGN KEY (shoppinglistid) REFERENCES shoppinglists(shoppinglistid) ON DELETE CASCADE;
+COMMIT;

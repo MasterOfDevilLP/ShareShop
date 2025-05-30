d7af5402f1025ef03afcd9711971b6a1de2fe006
Huyen Bao Han Nguyen <s87044@informatik.htw-dresden.de>
2025-05-30 11:39:08 +0200

    create add product popup

--- src/frontend/Einkaufsliste/app.js
+++ src/frontend/Einkaufsliste/app.js
@@ -1,3 +1,81 @@
+new Vue({
+    el: '#app',
+    data: {
+     products: [
+      { id: "1", kategorie: "Obst", name: "Äpfel", preis: "", datum: "12/2/2025", menge: "200", einheit: "gramm" },
+      { id: "2", kategorie: "Gemüse", name: "Tomaten", preis: "", datum: "12/2/2025", menge: "3", einheit: "stücke" },
+      { id: "3", kategorie: "Getränke", name: "Orangensaft", preis: "", datum: "12/2/2025", menge: "1", einheit: "liter" },
+      { id: "4", kategorie: "Fleisch", name: "Hähnchenbrust", preis: "", datum: "12/2/2025", menge: "500", einheit: "gramm" },
+      { id: "5", kategorie: "Backwaren", name: "Brot", preis: "", datum: "12/2/2025", menge: "1", einheit: "stück" }
+      ],
+    
+    newProduct: {
+      name: '',
+      kategorie: '',
+      menge: '',
+      einheit:''
+    },
+  
+    wg_name: 'Wocheneinkauf',
+    showPopup:false
+    },
+
+    
+    methods: {
+      zuruck_startseite(){
+         window.location.href = '../Startseitendesign/startseite.html';
+      },
+
+      getIcon(kategorie) {
+        const icons = {
+          Obst: 'Icons/obst.png',
+          Gemüse: 'Icons/gemuese.png',
+          Getränke: 'Icons/getraenke.png',
+          Fleisch: 'Icons/fleisch.png',
+          Backwaren: 'Icons/brot.png'
+        };
+      return icons[kategorie] || 'Icons/default.png';
+      },
+
+      add_product() {
+      this.showPopup = true;
+      },
+
+      closePopup() {
+        this.showPopup = false;
+        this.resetNewProduct();
+      },
+      resetNewProduct() {
+            this.newProduct = { name: '', kategorie: '', menge: '', einheit: '' };
+      },
+
+      saveProduct() {
+        if(!this.newProduct.name || !this.newProduct.kategorie || !this.newProduct.menge || !this.newProduct.einheit) 
+          {
+          alert('Bitte alle Felder ausfüllen.');
+          return;
+          }
+
+        this.products.push({
+          id: Date.now().toString(),
+          name: this.newProduct.name,
+          kategorie: this.newProduct.kategorie,
+          datum: new Date().toLocaleDateString(),
+          menge: this.newProduct.menge,
+          einheit: this.newProduct.einheit,
+          preis: null
+        });
+
+        this.closePopup();
+      },
+
+      deleteProduct(id){
+        this.products = this.products.filter(product => product.id !== id);
+      }
+  }
+});
+
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(registration => {
@@ -7,3 +85,4 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+

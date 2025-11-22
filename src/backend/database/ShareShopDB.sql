cdd8cdd357e6bfb107d82720d7adc9e771c54288
MasterOfDevilLP <flosue2017@gmail.com>
2025-11-22 14:22:52 +0100

    added Invites Table to DB + Invite Object Class representing it -> current worktime ~45min -> manually tested functionallity in this commit -> still needs handling inside WG Object

--- src/backend/database/ShareShopDB.sql
+++ src/backend/database/ShareShopDB.sql
@@ -410,3 +410,22 @@ ALTER TABLE ONLY public.users
     DROP CONSTRAINT users_wgid_fkey;
 ALTER TABLE ONLY public.users
     DROP COLUMN wgid;
+
+-- adding Invites Table
+CREATE TABLE public.invites (
+    token uuid NOT NULL,
+    wgid uuid NOT NULL,
+    userid uuid,
+    creationDateTime TIMESTAMP NOT NULL,
+    expiryDateTime TIMESTAMP
+);
+
+-- adding constraints
+ALTER TABLE public.invites OWNER TO backendusr;
+ALTER TABLE ONLY public.invites
+    ADD CONSTRAINT invites_pkey PRIMARY KEY (token);
+ALTER TABLE ONLY public.invites
+    ADD CONSTRAINT invites_wgid_fkey FOREIGN KEY (wgid) REFERENCES public.wg(wgid) ON DELETE CASCADE;
+ALTER TABLE ONLY public.invites
+    ADD CONSTRAINT invites_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
+        

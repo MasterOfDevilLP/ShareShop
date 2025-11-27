17a5cecc25b2de53018db53646c1c03178b02511
Emil Hempel <s86451@htw-dresden.de>
2025-11-27 18:49:02 +0100

    invite endpoints

--- src/backend/src/main/java/shareshop/rest/RestUtils.java
+++ src/backend/src/main/java/shareshop/rest/RestUtils.java
@@ -1,5 +1,6 @@
 package shareshop.rest;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -15,8 +16,10 @@ import io.javalin.http.HttpStatus;
 import jakarta.servlet.http.HttpSession;
 import shareshop.AppContext;
 import shareshop.User;
+import shareshop.WG;
 import shareshop.rest.requests.ErrorResponse;
 
+// a collection of functions frequently needed for REST endpoints
 public class RestUtils {
 	public static User getAuthorizedUser(Context ctx) {
 		
@@ -46,5 +49,43 @@ public class RestUtils {
 		ctx.contentType(ContentType.JSON);
 		ctx.result(gson.toJson(new ErrorResponse(message)));
 		ctx.status(status);
+	}
+	
+	// retrieves a UUID from a path parameter
+	// returns null and sets a response error if this fails
+	public static UUID getPathParamUUIDSafe(Context ctx, String key) {
+		String id = ctx.pathParam(key);
+		try {
+			UUID uuid = UUID.fromString(id);
+			return uuid;
+		} catch(IllegalArgumentException e) {
+			setResponseError(ctx, HttpStatus.BAD_REQUEST, "malformed UUID");
+			return null;
+		}
+	}
+	
+	public static WG getWGAsMember(Context ctx, UUID wgid, User user, AppContext appCtx) {
+		Logger logger = LoggerFactory.getLogger(RestUtils.class);
+		try {
+			if(!user.isUserInWG(wgid)) {
+				// wrong WG
+				logger.debug("wrong WG. Expected {}, got {}", user.getWgIDList().toString(), wgid);
+				RestUtils.setResponseError(ctx, HttpStatus.UNAUTHORIZED, "incorrect WG");
+				return null;
+			}
+			
+			WG wg = appCtx.wgManager.getWG(wgid);
+			if(wg == null) {
+				// no such WG exists, respond with 401 to not leak information about which ones exist and which don't
+				logger.debug("no such WG");
+				RestUtils.setResponseError(ctx, HttpStatus.UNAUTHORIZED, "incorrect WG");
+				return null;
+			}
+			return wg;
+		} catch(SQLException e) {
+			// no idea why
+			e.printStackTrace();
+			return null;
+		}
 	}
 }

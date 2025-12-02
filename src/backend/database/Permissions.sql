d3ecc4cef5fbd500f5232a9ab179cf36292ac9f8
MasterOfDevilLP <flosue2017@gmail.com>
2025-12-02 18:15:58 +0100

    fixed migration script to give each wg one owner

--- src/backend/database/Permissions.sql
+++ src/backend/database/Permissions.sql
@@ -1,4 +1,19 @@
-BEGIN TRANSACTION;
+BEGIN;
+    -- adding the owner flag
     ALTER TABLE ONLY public.userallocation
         ADD owner_flag BOOLEAN DEFAULT false NOT NULL;
+COMMIT;
+
+BEGIN;
+    -- making one of the users inside a wg owner
+	UPDATE public.userallocation
+	SET owner_flag = true
+	WHERE userid IN (
+		SELECT userid
+		FROM (
+			SELECT userid, wgid, ROW_NUMBER() OVER (PARTITION BY wgid ORDER BY joindate ASC) as rn
+			FROM public.userallocation
+		) AS sub
+		WHERE rn = 1
+	);
 COMMIT;

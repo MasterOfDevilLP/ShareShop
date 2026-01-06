bb54c64db3e6d8cf0ee6dfb84b0c9c2def0caa7d
Huyen Bao Han Nguyen <s87044@informatik.htw-dresden.de>
2026-01-06 15:39:45 +0100

    split einkaufmodus from genreral einkauf

--- src/frontend/Einkaufsliste/app.js
+++ src/frontend/Einkaufsliste/app.js
@@ -137,6 +137,15 @@ new Vue({
     },
   },
 
+  watch: {
+  localTicks: {
+    handler(val) {
+      localStorage.setItem("localTicks", JSON.stringify(val));
+    },
+    deep: true,
+  }
+},
+
   mounted() {
     /**
      * Lädt initial die WG-Produkte und die Einkaufsliste, falls IDs vorhanden.
